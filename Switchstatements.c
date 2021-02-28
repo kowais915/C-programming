085f640d8b0b48e1f0bcea0fc78757efb414e5f7
kowais915 <kowais915@gmail.com>
2021-02-28 20:34:44 +0500

    practicing switch statements in c

---
+++ Switchstatements.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+// int main(){
+// printf("Owais ");   
+// printf("Khan");
+// printf(" Hello! \n");
+
+// }
+// // practicing Switch statements in C
+
+int main() 
+{
+
+char name = 'O';
+
+switch (name) 
+{
+case 'M':
+printf("Nah Man!");
+    break;
+case 'O':
+printf("Yeehaw!");
+    break;
+
+case 'N':
+printf("Nopes");
+    break;
+
+
+default:
+    break;
+}
+}
