9d4f105ad50227008befddc0de14b931c3780b69
kowais915 <kowais915@gmail.com>
2021-03-02 22:29:26 +0500

    Corrected the function by passing addresses

--- functions.c
+++ functions.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
 
-void set_southwest(int lat, int lon){
-    lat = lat + 1;
-    lon = lon - 1;
+// void set_southwest(int lat, int lon){
+//     lat = lat + 1;
+//     lon = lon - 1;
 
-}
-int main(){
-    int latitude = 34;
-    int longitude = 45;
+// }
+// int main(){
+//     int latitude = 34;
+//     int longitude = 45;
 // passing the function LOCAL VARIABLES
-    set_southwest(latitude, longitude);
+//     set_southwest(latitude, longitude);
 
-    printf("%i, %i", latitude, longitude);
+//     printf("%i, %i", latitude, longitude);
+/* Though we expect the value of latitude to be decreased while that of longitude to be increased.
+But the values did not change. That is because the function copies the values of the local variables into the lat and lon. As a result, the local variables of set_southwest is changed.
+*/
+  void set_southwest(int *lat, int *ion){
+        *lat = *lat - 1;
+        *ion = *ion + 1;
+
+    }
+int main(){
+  
+int latitude = 34;
+int longitude = 45;
+set_southwest(&latitude, &longitude);
+
+printf("The latest values of latitude is:  %i and longitude %i", latitude, longitude);
+
+
+   
 
 }
