70c70824d1cc774cc15c7c651bef9ea7e2aac7db
kowais915 <kowais915@gmail.com>
2021-03-03 03:37:43 +0500

    Practicing Arrays in C

--- Arrays.c
+++ Arrays.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
-
+int j = 0;
 int main(){
-    char name[20] = "Owais";
-    int age = 20;
+   int a[5] = {1, 2, 3 ,4 ,5};
+   for(int i = 0; i < 5; i++)
+   {
+       scanf("%d", &a[i]);
+   }
+printf("The numbers in the Array are changed to: %d\n", a[j]);
+for(j = 0; j<5; j++){
+printf("%d", a[j]);
+   }
 
-   printf("%s is %d years old.", name, age);
 }
