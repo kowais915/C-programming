73197e97656cbe4ba940bbf302b5d06fe4306e19
kowais915 <kowais915@gmail.com>
2021-03-06 12:27:51 +0500

    Array varaibles are pointers

--- stringinc.c
+++ stringinc.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
+//string in c are just arrays of type char
+//If you want to pass a string to a function in c, you must do it via char arrays
+void coolMessage(char* msg){
+
+printf("Message reads %s: ", msg);
+}
+
 int main(){
-    // int x = 1;
-    //printing the address of the stored data
+int msg[] = "Hello, how are you?";
 
-    // printf("The address of the variable is %p: ", &x);
-
-char codes[2];
-scanf("%d", codes);
-printf("%d", codes[0]);
-
+coolMessage(msg);
 
 }
