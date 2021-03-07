f38df401f650dbec55016a5a8e1e683349abd989
kowais915 <kowais915@gmail.com>
2021-03-07 14:48:13 +0500

    Practicing pointers

--- Arrays.c
+++ Arrays.c
@@ -4,21 +4,25 @@
 
 
 int main(){
-  int a[5];
+//   int a[5];
 
-  printf("Please enter the values: \n");
-  int i;
+//   printf("Please enter the values: \n");
+//   int i;
 
-  for(i = 0; i< 5; i++){
-     scanf("%i", &a[i]);
-  }
-int j;
-  for (j = 0; j < 5; j++)
-  {
-     printf("The new entries of the array are: %i\n", a[j]);
+//   for(i = 0; i< 5; i++){
+//      scanf("%i", &a[i]);
+//   }
+// int j;
+//   for (j = 0; j < 5; j++)
+//   {
+//      printf("The new entries of the array are: %i\n", a[j]);
 
-  }
+//   }
 
+int num = 4;
+int *addofnum = &num;
+int address = *addofnum;
+printf("The address of the variable is: %p", address);
 
 }
 
