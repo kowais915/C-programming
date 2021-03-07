40778a9794d32af16209d210d998a61af7ee1c8f
kowais915 <kowais915@gmail.com>
2021-03-08 01:51:30 +0500

    Practicing arrays in C

--- stringinc.c
+++ stringinc.c
@@ -2,14 +2,12 @@
 
 //string in c are just arrays of type char
 //If you want to pass a string to a function in c, you must do it via char arrays
-void coolMessage(char* msg){
 
-printf("Message reads %s: ", msg);
+void message(char msg[]){
+    printf("The message reads: %s", msg);
+
 }
-
 int main(){
-int msg[] = "Hello, how are you?";
-
-coolMessage(msg);
-
+char greetings[] = "Eid Mubarak!";
+message(greetings);
 }
