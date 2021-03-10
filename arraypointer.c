97c481ef0be830a96599a986955c1798a2f388f7
kowais915 <kowais915@gmail.com>
2021-03-10 08:27:10 +0500

    practicing char arrays and strings in c

---
+++ arraypointer.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+char name[] = "Owais";
+char *arraypoint = name;
+
+printf("The array pointer reads: %p", arraypoint);
+}
--- stringinc.c
+++ stringinc.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
-
-//string in c are just arrays of type char
-//If you want to pass a string to a function in c, you must do it via char arrays
-
-void message(char msg[]){
-    printf("The message reads: %s", msg);
-
+void str(char string[]){
+    printf("The string reads: %s", string);
 }
+
 int main(){
-char greetings[] = "Eid Mubarak!";
-message(greetings);
+    char strrr[] = "Hey, how is it going?";
+    str(strrr);
 }
