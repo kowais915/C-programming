ae39214770b418f27a14ded82763edab9a034923
kowais915 <kowais915@gmail.com>
2021-03-20 15:36:40 +0500

    Changing values of the member values in a struct using pointers

--- RandomCode/changes.c
+++ RandomCode/changes.c
@@ -1,23 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct reptiles{
+// typedef struct reptiles{
+// const char *name;
+// int age;
+// } reptiles;
+
+
+// void bd(reptiles *animal){
+//     (*animal).age = (*animal).age +1;
+//     printf("Happy birthday! You are now: %d", (*animal).age);
+
+
+// }
+
+
+typedef struct student {
 const char *name;
+int reg;
 int age;
-} reptiles;
+float gpa;
+} students;
 
 
-void bd(reptiles *animal){
-    (*animal).age = (*animal).age +1;
-    printf("Happy birthday! You are now: %d", (*animal).age);
-
-
+//the happy birthday function
+void bd(students *std){
+(*std).age = (*std).age + 1;
+printf("Happy Birthday, Owais! You are now: %d", (*std).age);
 }
 
 int main(){
-reptiles turtle;
+// reptiles turtle;
 
-turtle.age = 45;
-bd(&turtle);
+// turtle.age = 45;
+// bd(&turtle);
+
+// student owais = {"Owais", 2019249, 20, 3.4 };
+
+students owais;
+owais.age = 20;
+//calling the happy birthday functions
+bd(&owais);
 
 }
