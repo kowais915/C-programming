b47a5528c9f80efdb56d78b8690819a061d6b13f
kowais915 <kowais915@gmail.com>
2021-03-28 23:45:29 +0500

    taking data from text file, inserting it into a linkedlist and then reversing the list. It has some issue...

--- ClassAssignments/ao2/task2/t2.c
+++ ClassAssignments/ao2/task2/t2.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct node{
-    int integers;
-    struct node *next;
+typedef struct nodeorg{
+    char *integers;
+    struct nodeorg *next;
 
-} node;
+} nodeorg;
 
-void reverseLink(node **head){
-    node *previous = NULL;
-    node *next = NULL;
-    node *curr = *head;
+typedef struct numbers{
+    int data;
+    struct numbers *next;
+} numbers;
+
+void reverseLink(numbers **head){
+    numbers *previous = NULL;
+    numbers *next = NULL;
+    numbers *curr = *head;
 
     while (curr!=NULL)
     {
@@ -23,45 +29,114 @@ void reverseLink(node **head){
 }
 
 //the function that prints the linkedlist
-void print(node *start){
-node *i = start;
+void print(numbers *start){
+numbers *i = start;
 while (i!=NULL)
 {
-    printf("%d ", i->integers);
+    printf("%d ", i->data);
     i = i->next;
 }
 
 
 
 }
-int main(){
-node *head =NULL;
+int main(int arga, char *argb[]){
 
-node num1;
-node num2;
-node num3;
-node num4;
-num1.integers = 2;
-head = &num1;
+//Making sure the user inputs at least two arguments
+if(arga!=2){
+printf("Please provide two arguments!");
+}
+
+//Defining a pointer of type FILE
+    FILE *fp;
+
+
+//using the fopen function along with "r" which is a mode and stands for read
+fp = fopen(argb[1], "r");
+
+//Error Handling
+if(fp == NULL){
+     fprintf(stderr, "You encountered an error!");
+    exit(1);
+}
+  
+//maintaing two pointers i.e head for the start and current for the current
+//both the pointers are NULL
+nodeorg *current =NULL;
+nodeorg *head =NULL;
+char line[200];
+
+while (fgets(line, sizeof(line), fp))
+{
+    nodeorg *dyna = malloc(sizeof(nodeorg));
+    dyna->integers = strdup(line);
+    dyna->next =NULL;
+
+      if(head == NULL){
+            current = dyna;
+            head= dyna;
+        } else {
+            current = current->next;
+            current->next= dyna;
+        }
+}
+fclose(fp);
+
+
+//maintaining a pointer for the reverse function
+
+//printing the inserted data that was inserted into the linkedlist
+  for(current = head; current ; current=current->next){
+        printf("Data from the text file has been loaded to a linked insert\n");
+        printf("%s\n", current->integers);
+    
+
+    }
+
+    
+
+numbers *head1 =NULL;
+
+numbers num1;
+numbers num2;
+numbers num3;
+numbers num4;
+numbers num5;
+numbers num6;
+
+
+head1 = &num1;
+num1.data = 5;
 num1.next = &num2;
 
 
-num2.integers = 3;
+num2.data = 1;
 num2.next =&num3;
 
 
-num3.integers = 4;
+num3.data = 2;
 num3.next =&num4;
 
 
-num4.integers = 5;
-num4.next =NULL;
+num4.data = 3;
+num4.next =&num5;
 
-reverseLink(&head);
+num5.data = 4;
+num5.next = &num6;
+
+num6.data = 5;
+num6.next = NULL;
+
+reverseLink(&head1);
 printf("The linked has been reversed\n");
 printf("Printing the reversed list\n");
 
-print(head);
+print(head1);
+
+
+// reverseLink(&current);
+
+// printf("%s ", head->integers);
 
 
 return 0;
