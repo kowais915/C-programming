13a55d3ed4005a43290b2f4bb5072e002fad9019
kowais915 <kowais915@gmail.com>
2021-04-30 05:30:52 +0500

    practicing linked lists

---
+++ RandomCode/DataStructures/linkedlists/link.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node{
+    int data;
+    struct Node *next;
+
+} node;
+
+
+
+
+
+
+int main(){
+node *head = (node*)malloc(sizeof(node));
+head->data = 23;
+head->next = NULL;
+
+printf("The data in the struct is: %d", head->data);
+printf("The address in the struct is: %d", head);
+
+
+
+
+}
