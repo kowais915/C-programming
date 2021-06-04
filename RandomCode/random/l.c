b98d3ce78f2b1c4c43f2c0d7d8ad2be48d41d8f5
kowais915 <kowais915@gmail.com>
2021-06-04 05:25:19 +0500

    inserted a node between two pre-exisitng nodes

--- RandomCode/random/l.c
+++ RandomCode/random/l.c
@@ -26,7 +26,7 @@ int main(){
 
 //creating three nodes
 
-node node1, node2, node3;
+node node1, node2, node3, node4;
 //introducing a head pointer to keep track of the head of linkedlist
 node *head;
 
@@ -35,13 +35,21 @@ node *head;
 node1.val =5;
 node2.val =6;
 node3.val=7;
+node4.val = 20;
 
 //linking these nodes
 
 head=&node1;//storing the address of the first node in the head pointer
 node1.next=&node2;
-node2.next=&node3;
+node2.next = &node4;
+node4.next=&node3;
 node3.next=NULL; //the end of the linked list
+
+//5 -> 6 -> 7
+
+//wait I need to insert 20 after 6 and before 7 so that it looks:
+//5->6->20->7
+
 
 
 
